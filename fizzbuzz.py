d93d77e4a6958d11a99e98270dc7f194e7445852
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2021-12-28 17:58:50 -0600

    final changes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,7 @@
 # coding the FizzBuzz game as an exercise for Tom Scott's video:
 # https://youtu.be/QPZ0pIK_wsc
 #
+# Also serves as my introduction to Python
 """
  Created on Tue Dec 28 2021
 
@@ -33,7 +34,7 @@ def play() :
 	return 0
 
 # this is my second attempt after watching the rest of Tom's video
-# note the efficiency and cleanliness of the code, this is much better.
+# note the efficiency and cleanliness of the code; this is much better.
 def playBetter() :
 	current = 1
 	while(current <= maximum) :
@@ -46,5 +47,6 @@ def playBetter() :
 			output = current
 		print(output)
 		current+=1
+	return 0
 
 main()
